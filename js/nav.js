3000932ce849e25dc1739815ac2dfb728605121d
haistous <vlad102013@mail.ru>
2022-10-23 17:02:44 +0300

    Add nav scrolling.

---
+++ js/nav.js
@@ -0,0 +1,29 @@
+document.addEventListener('DOMContentLoaded', function () {
+
+    const navInit = () => {
+        const nav = document.querySelector('.sidebar-nav');
+        const links = document.querySelectorAll('.sidebar-nav__li');
+        const sections = document.querySelectorAll('.section');
+        sections.forEach(section => {
+            if (window.pageYOffset >= section.offsetTop) {
+                links.forEach(link => {
+                    link.classList.remove('sidebar-nav__active');
+                    console.log(link);
+                    if (link.dataset.section === section.dataset.section) {
+                        link.classList.add('sidebar-nav__active')
+                    }
+
+                })
+            }
+
+        })
+    }
+    navInit()
+    window.addEventListener('scroll', () => {
+        navInit();
+    })
+    window.addEventListener('resize', () => {
+        navInit();
+    })
+
+})
