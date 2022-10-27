07fbe6d5b42dc057639fee83f2ade5bdc3d3c83a
haistous <vlad102013@mail.ru>
2022-10-27 15:35:46 +0300

    Fixed bug with nav.

--- js/nav.js
+++ js/nav.js
@@ -5,7 +5,8 @@ document.addEventListener('DOMContentLoaded', function () {
         const links = document.querySelectorAll('.sidebar-nav__li');
         const sections = document.querySelectorAll('.section');
         sections.forEach(section => {
-            if (window.pageYOffset >= section.offsetTop) {
+            if (window.pageYOffset + 50 >= section.offsetTop) {
+                console.log(window.pageYOffset, section.offsetTop)
                 links.forEach(link => {
                     link.classList.remove('sidebar-nav__active');
                     if (link.dataset.section === section.dataset.section) {
